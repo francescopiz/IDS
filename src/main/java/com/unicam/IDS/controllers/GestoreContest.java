caa7d40d861088099231073c32a288ccde25a8e7
francescopiz <francy.pizzuto@gmail.com>
2024-08-31 16:06:04 +0200

    [edit] aggiunti tag su Comune, POI, Contenuto, Evento, Posizione, Utente [edit] GestoreContest, GestoreRuoli, GestoreUtenti

--- src/main/java/com/unicam/IDS/controllers/GestoreContest.java
+++ src/main/java/com/unicam/IDS/controllers/GestoreContest.java
@@ -1,52 +1,52 @@
 package com.unicam.IDS.controllers;
 
-import com.unicam.IDS.models.approvabili.Contenuto;
 import com.unicam.IDS.models.Contest;
 import com.unicam.IDS.models.Iscrizione;
-import com.unicam.IDS.models.Liste;
 import com.unicam.IDS.models.ruoli.Utente;
+import com.unicam.IDS.repositorys.ContestRepository;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 public class GestoreContest {
 
-    private Liste repository;
+    private ContestRepository contestRepository;
 
-    public GestoreContest(Liste liste){
-        repository = liste;
+    public GestoreContest(ContestRepository contestRepository) {
+        this.contestRepository = contestRepository;
     }
 
 
-    /*public boolean addContest(String nome, String descrizione, Utente animatore){
-        return repository.addContest(nome, descrizione, animatore);
-    }
-
-    public boolean removeContest (int idContest){
-        return repository.removeContest(idContest);
-    }
-
-    public boolean iscriviti(int contestId, int utenteId, Contenuto contenuto) {
-
-        Contest contest = repository.getContest(contestId);
-        if (contest == null) {
-            return false;
-        }
-            List<Integer> utentiIscritti = contest.getListaIscritti();
-            List<Integer> utentiAmmessi = contest.getListaAmmessi();
-            if(utentiIscritti.contains(utenteId))
-                return false;
-            if(utentiAmmessi.size() > 0)
-                if (!utentiAmmessi.contains(utenteId))
-                    return false;
-            Iscrizione iscrizione = new Iscrizione(contestId, utenteId, contenuto);
-            repository.addIscrizione(iscrizione);
-            contest.iscriviUtente(utenteId);
-
+    public boolean addContest(Contest contest) {
+        try {
+            contestRepository.save(contest);
             return true;
+        } catch (Exception ignored) {
+        }
+        return false;
     }
 
-    public boolean decretaVincitoreContest (int idContest, Iscrizione iscrizione){
+    public Contest getContest(int idUtente) {
+        return contestRepository.findById(idUtente).orElse(null);
+    }
 
-        return repository.getContest(idContest).setVincitore(iscrizione);
-    }*/
+    public boolean deleteContest(Contest contest) {
+        if (contestRepository.existsById(contest.getId())) {
+            this.contestRepository.delete(contest);
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean iscriviti(Contest contest, Utente utente, MultipartFile file) {
+        if (contest.utenteAbilitato(utente)) {
+            contest.addIscrizione(new Iscrizione(utente, contest, file));
+            return true;
+        }
+        return true;
+    }
+
+    public boolean decretaVincitoreContest(Contest contest, Iscrizione iscrizione) {
+        return contest.setVincitore(iscrizione);
+    }
 }
