2dcd7dadd7e5d0de2dc7b6547e065dc9ecc8a270
guglielmo <guglielmogio.grifoni@studenti.unicam.it>
2024-08-31 13:02:50 +0200

    [Update] Contest, Iscrizione, pom

--- src/main/java/com/unicam/IDS/models/Contest.java
+++ src/main/java/com/unicam/IDS/models/Contest.java
@@ -1,84 +1,158 @@
 package com.unicam.IDS.models;
 
 import com.unicam.IDS.models.ruoli.Utente;
+import jakarta.persistence.*;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+/**
+ * La classe Contest rappresenta un concorso con un nome, una descrizione, un animatore e una lista di iscrizioni.
+ */
+@Entity
 public class Contest {
-
-    private String descrizione;
+    @Id
+    @GeneratedValue
     private int id;
     private String nome;
+    private String descrizione;
+    @ManyToOne
     private Utente animatore;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = false)
+    private Set<Iscrizione> listaIscritti;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = false)
+    private List<Utente> utentiAmmessi;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Iscrizione vincitore;
-    private List<Integer> utentiIscritti;
-    private List<Integer> utentiAmmessi;
 
-    Contest(int id, String nome, String descrizione, Utente animatore) {
-        this.id = id;
+    /**
+     * Costruttore della classe Contest.
+     *
+     * @param nome        Il nome del concorso.
+     * @param descrizione La descrizione del concorso.
+     * @param animatore   L'animatore del concorso.
+     */
+    public Contest(String nome, String descrizione, Utente animatore) {
         this.nome = nome;
         this.descrizione = descrizione;
         this.animatore = animatore;
         this.vincitore = null;
-        this.utentiIscritti = new ArrayList<>();
         this.utentiAmmessi = new ArrayList<>();
+        this.listaIscritti = new HashSet<>();
     }
-    Contest(int id, String nome, String descrizione, Utente animatore, List<Integer> utentiAmmessi){
-        this.id = id;
+
+    /**
+     * Costruttore della classe Contest con lista di utenti ammessi.
+     *
+     * @param nome          Il nome del concorso.
+     * @param descrizione   La descrizione del concorso.
+     * @param animatore     L'animatore del concorso.
+     * @param utentiAmmessi La lista di utenti ammessi al concorso.
+     */
+    public Contest(String nome, String descrizione, Utente animatore, List<Utente> utentiAmmessi) {
         this.nome = nome;
         this.descrizione = descrizione;
         this.animatore = animatore;
         this.vincitore = null;
-        this.utentiIscritti = new ArrayList<>();
         this.utentiAmmessi = utentiAmmessi;
+        this.listaIscritti = new HashSet<>();
     }
 
-    public List<Integer> getListaAmmessi(){
-        if (this.utentiAmmessi.size() > 0)
-            return utentiAmmessi;
-        return new ArrayList<>();
+    /**
+     * Costruttore di default della classe Contest.
+     */
+    public Contest() {
+        this.utentiAmmessi = new ArrayList<>();
+        this.listaIscritti = new HashSet<>();
     }
 
-
-    public int getId() {
-        return id;
-    }
-
-    public List<Integer> getListaIscritti(){
-        return utentiIscritti;
-    }
-
-    public boolean contestPrivato(){
-        return this.utentiAmmessi.size() > 0;
-    }
-
-    public Iscrizione getVincitore() {
-        return this.vincitore;
-    }
-
-    public boolean setVincitore(Iscrizione iscrizione){
-        this.vincitore = iscrizione;
-        return true;
-    }
-    
-    //TODO in qualche modo deve restituire false? I vari controlli sono fatti sul gestore, bisogna metterli lì o qui
-    public boolean iscriviUtente(int idUtente){
-        utentiIscritti.add(idUtente);
+    /**
+     * Aggiunge un'iscrizione alla lista delle iscrizioni.
+     *
+     * @param iscrizione L'iscrizione da aggiungere.
+     * @return true se l'iscrizione è stata aggiunta con successo, false altrimenti.
+     */
+    public boolean addIscrizione(Iscrizione iscrizione) {
+        if (!utenteAbilitato(iscrizione.getIscritto()))
+            return false;
+        listaIscritti.add(iscrizione);
         return true;
     }
 
     /**
-     * Metodo per vedere se un utente può iscriversi o meno a un contest
-     * @param utente
-     * @return true se l'utente può iscriversi
-     * @return false se non si può iscrivere (nel caso in cui il contest sia privato e non ci sta scritto il suo nome)
+     * Restituisce la lista degli utenti ammessi.
+     *
+     * @return La lista degli utenti ammessi.
      */
-    public boolean utenteAbilitato (Utente utente){
-        if(this.utentiAmmessi == null || this.utentiAmmessi.size() == 0)
-            return true;
-        return this.utentiAmmessi.contains(utente.getId());
+    public List<Utente> getUtentiAmmessi() {
+        return utentiAmmessi;
     }
 
+    /**
+     * Restituisce l'ID del concorso.
+     *
+     * @return L'ID del concorso.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Restituisce la lista delle iscrizioni.
+     *
+     * @return La lista delle iscrizioni.
+     */
+    public Set<Iscrizione> getListaIscritti() {
+        return listaIscritti;
+    }
+
+    /**
+     * Verifica se il concorso è privato.
+     *
+     * @return true se il concorso è privato, false altrimenti.
+     */
+    public boolean contestPrivato() {
+        return !this.utentiAmmessi.isEmpty();
+    }
+
+    /**
+     * Restituisce il vincitore del concorso.
+     *
+     * @return Il vincitore del concorso.
+     */
+    public Iscrizione getVincitore() {
+        return this.vincitore;
+    }
+
+    /**
+     * Imposta il vincitore del concorso.
+     *
+     * @param iscrizione L'iscrizione del vincitore.
+     * @return true se il vincitore è stato impostato con successo, false altrimenti.
+     */
+    public boolean setVincitore(Iscrizione iscrizione) {
+        if (this.vincitore != null)
+            return false;
+        this.vincitore = iscrizione;
+        return true;
+    }
+
+    /**
+     * Verifica se un utente è abilitato a partecipare al concorso.
+     *
+     * @param utente L'utente da verificare.
+     * @return true se l'utente è abilitato, false altrimenti.
+     */
+    public boolean utenteAbilitato(Utente utente) {
+        if (this.utentiAmmessi == null || this.utentiAmmessi.isEmpty())
+            return true;
+        return this.utentiAmmessi.contains(utente);
+    }
+
+    //TODO: implementare equals, toString, hashcode
 }
--- src/main/java/com/unicam/IDS/models/Iscrizione.java
+++ src/main/java/com/unicam/IDS/models/Iscrizione.java
@@ -1,28 +1,64 @@
 package com.unicam.IDS.models;
 
-import com.unicam.IDS.models.approvabili.Contenuto;
+import com.unicam.IDS.models.ruoli.Utente;
+import jakarta.persistence.*;
+import org.springframework.web.multipart.MultipartFile;
 
+@Entity
 public class Iscrizione {
-    private int idUtente;
-    private int idContest;
-    private com.unicam.IDS.models.approvabili.Contenuto Contenuto;
 
-    public Iscrizione(int c, int u,  Contenuto co){
-        idUtente = u;
-        idContest = c;
-        Contenuto = co;
+    @Id
+    @GeneratedValue
+    private int id;
+    @ManyToOne
+    private Utente iscritto;
+    @ManyToOne
+    private Contest contest;
+    @Transient
+    private MultipartFile file;
+
+    private String descrizione;
+
+
+    public Iscrizione(Utente iscritto, Contest contest, MultipartFile file) {
+        this.iscritto = iscritto;
+        this.contest = contest;
+        this.file = file;
     }
 
-    public int getIdContest(){
-        return idContest;
+    public Iscrizione(Utente iscritto, Contest contest, String descrizione) {
+        this.descrizione = descrizione;
     }
 
-    public int getIdUtente(){
-        return idUtente;
+    public Iscrizione() {
     }
 
-    public Contenuto getContenuto(){
-        return Contenuto;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Iscrizione that)) return false;
+        return iscritto.equals(that.iscritto) && contest.equals(that.contest);
     }
 
+    @Override
+    public int hashCode() {
+        return iscritto.hashCode() + contest.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Iscrizione{" +
+                "iscritto=" + iscritto.toString() +
+                ", contest=" + contest.toString() +
+                ", file=" + file +
+                '}';
+    }
+
+    public Utente getIscritto() {
+        return iscritto;
+    }
+    public Contest getContest() {
+        return contest;
+    }
 }
